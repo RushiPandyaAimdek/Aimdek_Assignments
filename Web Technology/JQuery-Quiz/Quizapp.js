2fe79fcf272074645cc0629a9f855d9ecbcfb515
Rushi <rushi.pandya@aimdek.com>
2022-02-13 13:20:42 +0530

    updated

--- Web Technology/JQuery-Quiz/Quizapp.js
+++ Web Technology/JQuery-Quiz/Quizapp.js
@@ -1,48 +1,52 @@
 // variable declaration
 let i = 0;
-let correct_answer = 0;
 let mark = 0;
 
 // questions list
 let data = [
     {
         "Question": "1) Which city is called 'Rangilu' from below options ?",
-        "Option1": "Ahmedabad",
-        "Option2": "Rajkot",
-        "Option3": "Surat",
-        "Option4": "Baroda",
+        "Option": [
+            "Ahmedabad",
+            "Rajkot",
+            "Surat",
+            "Baroda"],
         "Answer": "Rajkot"
     },
     {
         "Question": "2) Which country has largest democracy in the world ?",
-        "Option1": "US",
-        "Option2": "UK",
-        "Option3": "India",
-        "Option4": "Pakistan",
+        "Option": [
+            "US",
+            "UK",
+            "India",
+            "Pakistan"],
         "Answer": "India"
     },
     {
         "Question": "3) What is height of The Mount Everest ?",
-        "Option1": "8848m",
-        "Option2": "8538m",
-        "Option3": "8748m",
-        "Option4": "8938m",
+        "Option": [
+            "8848m",
+            "8538m",
+            "8748m",
+            "8938m"],
         "Answer": "8848m"
     },
     {
         "Question": "4) Who invented arthashastra ?",
-        "Option1": "Brahmbhatt",
-        "Option2": "Nagarjuna",
-        "Option3": "Chanakya",
-        "Option4": "Amatya rakshas",
+        "Option": [
+            "Brahmbhatt",
+            "Nagarjuna",
+            "Chanakya",
+            "Amatya rakshas"],
         "Answer": "Chanakya"
     },
     {
         "Question": "5) Who is father of Indian Navy ?",
-        "Option1": "Maharana pratap",
-        "Option2": "Chhatrapati Shivaji Maharaj",
-        "Option3": "Pulkeshi second",
-        "Option4": "Porus",
+        "Option": [
+            "Maharana pratap",
+            "Chhatrapati Shivaji Maharaj",
+            "Pulkeshi second",
+            "Porus"],
         "Answer": "Chhatrapati Shivaji Maharaj"
     }
 ]
@@ -52,8 +56,9 @@ $(document).ready(function () {
     $('#btnFinish').hide();
     $('#result').hide();
     $('#quizContent').hide();
-    checkForSelect();
-    checkForFinish();
+    $('input:radio[name=option]').click(function () {
+        $('#btnnext').removeAttr('disabled');
+    })
 });
 
 // start button click
@@ -66,35 +71,28 @@ $(btnStart).click(function () {
 
 // next question button click
 $(btnnext).click(function () {
-    const checkval = $('input:radio[name=option]:checked').val();
-    makeResult(checkval, i);
+    // adding marks
+    if (data[i].Answer == data[i].Option[$('input:radio[name=option]:checked').val()]) mark += 5;
+
     i++;
     if (i == 4) {
-        $('#btnFinish').attr('disabled', 'true');
-        $('#btnFinish').show();
-        $('#btnnext').hide();
+        $("#btnnext").html('Finish');
         adding_questions(data, i);
-        checkForSelect();
+    }
+    else if (i == 5) {
+        $('#quizContent').hide();
+        $('.start_page').hide();
+        $('#result').show();
+        //making result
+        $('#marks').text(mark);
+        $('#correct_answer').text(mark / 5);
+        $('#percentage').text(((mark / 5) * 100 / 5));
     }
     else {
-        $('#btnFinish').hide();
         $('#quizContent').show();
         $('.start_page').hide();
         adding_questions(data, i);
-        checkForSelect();
     }
-});
-
-// finish button click
-$(btnFinish).click(function () {
-    const checkval = $('input:radio[name=option]:checked').val();
-    makeResult(checkval, i);
-    i++;
-    $('#finish').hide();
-    $('#quizContent').hide();
-    $('.start_page').hide();
-    $('#result').show();
-    displayResult();
 });
 
 // start-again button click
@@ -104,62 +102,14 @@ $(btnstart_again).click(function () {
 
 // for adding question to page
 function adding_questions(data, i) {
-    $('#questions').text(data[i].Question);
-    $('#option1').text(data[i].Option1);
-    $('#option2').text(data[i].Option2);
-    $('#option3').text(data[i].Option3);
-    $('#option4').text(data[i].Option4);
-    $('#number').text(i + 1);
-}
-
-// enable finish button after last question
-function checkForFinish() {
-    $('input:radio[name=option]').click(function () {
-        const checkval = $('input:radio[name=option]:checked').val();
-        $('#btnFinish').removeAttr('disabled');
-    });
-}
-
-// for displaying result
-function displayResult() {
-    $('#marks').text(mark);
-    $('#correct_answer').text(correct_answer);
-    $('#percentage').text((correct_answer*100)/5);
-}
-
-// validation whether option is selected or not
-function checkForSelect() {
+    // uncheck option and disable next button
     $('input:radio[name=option]').prop('checked', false);
     $('#btnnext').attr('disabled', 'true');
-    $('input:radio[name=option]').click(function () {
-        $('#btnnext').removeAttr('disabled');
-    })
-}
 
-// for making marks, percentage and correct answer
-function makeResult(checkval, i) {
-    if (checkval == "radio1") {
-        if ($('#option1').text() == data[i].Answer) {
-            mark += 5; 
-            correct_answer += 1;
-        }
-    }
-    else if (checkval == "radio2") {
-        if ($('#option2').text() == data[i].Answer) {
-            mark += 5; 
-            correct_answer += 1;
-        }
-    }
-    else if (checkval == "radio3") {
-        if ($('#option3').text() == data[i].Answer) {
-            mark += 5; 
-            correct_answer += 1;
-        }
-    }
-    else if (checkval == "radio4") {
-        if ($('#option4').text() == data[i].Answer) {
-            mark += 5;
-            correct_answer += 1;
-        }
-    }
+    $('#questions').text(data[i].Question);
+    $('#option1').text(data[i].Option[0]);
+    $('#option2').text(data[i].Option[1]);
+    $('#option3').text(data[i].Option[2]);
+    $('#option4').text(data[i].Option[3]);
+    $('#number').text(i + 1);
 }
